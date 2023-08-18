8b683085af75fa44829a31446ce63d5d3250af9f
Manav Verma <vermamanav117@gmail.com>
2023-08-18 21:52:00 +0530

    feat: Object program added, Installation in ReadMe added

---
+++ Basics/3-Objects.ts
@@ -0,0 +1,18 @@
+//Custom type in TS
+type User = {
+    email: string;
+    password: string;
+    isPaid: boolean
+}
+
+function createUser(user: User) {
+    console.log(user.email, user.password, user.isPaid);
+}
+
+const Manav = {
+    email: "vermamanav117@gmail.com",
+    password: "123456",
+    isPaid: true
+}
+
+createUser(Manav);
