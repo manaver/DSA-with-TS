ddedc14a946e955aa7acca240f91a8b12e7828e4
Manav Verma <vermamanav117@gmail.com>
2023-08-19 21:11:04 +0530

    feat: Return Object example added

--- Basics/3-Objects.ts
+++ Basics/3-Objects.ts
@@ -1,19 +1,30 @@
 //Custom type in TS
 type User = {
+    readonly _id: string;
     email: string;
     password: string;
-    isPaid: boolean
+    isPaid: boolean;
+    creditCard?: number; // ? means optional
 }
 
-function createUser(user: User) {
-    console.log(user.email, user.password, user.isPaid);
+function displayUser(user: User) {
+    console.log(user._id, user.email, user.password, user.isPaid);
 }
 
 const Manav = {
+    _id: "12",
     email: "vermamanav117@gmail.com",
     password: "123456",
-    isPaid: true
+    isPaid: true,
 }
 
-createUser(Manav);
+displayUser(Manav);
 
+// Example for Object returning
+function forgotPassword(user: User, newPassword: string): User {
+    user.password = newPassword;
+    return user;
+}
+
+let n2: User = forgotPassword(Manav, '12345678');
+console.log(n2);
